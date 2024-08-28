1c4606746631a51680bbfc382202aebfa7f497ce
sajeda swad <201047@ppu.edu.ps>
2024-08-29 00:40:32 +0300

    failing test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,11 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+// describe('template spec', () => {
+//   it('passes', () => {
+//     cy.visit('https://example.cypress.io')
+//   })
+// })
+//<reference type = "cypress"\> 
+describe('My First Test', () => {
+  it('Does not do much!', () => {
+    expect(true).to.equal(false)
   })
 })
