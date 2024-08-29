1474017453565e0e4dd626ee83f4afddd716bff3
sajeda swad <201047@ppu.edu.ps>
2024-08-29 12:47:12 +0300

    google search test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -4,8 +4,13 @@
 //   })
 // })
 //<reference type = "cypress"\> 
-describe('My First Test', () => {
-  it('Does not do much!', () => {
-    expect(true).to.equal(false)
-  })
+// const cypress = require("cypress")
+// describe('My First Test', () => {
+//   it('Does not do much!', () => { expect(true).to.equal(false)
+//   })
+// })
+it("Google search",()=>{
+cy.visit('http://google.com')
+cy.get('#APjFqb').type("automation step by step {Enter}")
+// cy.get('.aajZCb > .lJ9FBc > center > .gNO89b').click() click on google search button 
 })
