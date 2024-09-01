cf0bd375a21dbc93c9b29b8fe3e2d427b3b24239
sajeda swad <201047@ppu.edu.ps>
2024-09-01 12:54:55 +0300

    explicit assertion (assert&expect)

--- cypress/e2e/assertionDemo.cy.js
+++ cypress/e2e/assertionDemo.cy.js
@@ -1,20 +1,56 @@
 
 //implicit assertion
-it("assertion",()=>{
+
+// it("assertion",()=>{
+//     cy.visit('https://example.cypress.io')
+//     cy.wait(2000)
+//     cy.contains('get').click()
+//     cy.wait(2000)
+//     cy.get('#query-btn')
+//     .should('contain','Button')//should contain ->the text it have 
+//     .should('have.id','query-btn')//should have .class .id .text .html
+//     .should('be.visible')//should-be   .visible  .disabled be.focused == have.focus 
+//     //should-equal  .invoke('attr','id').should('equal','class-name')
+
+//     //and -> chained assertion 
+//     //instead of writing an should we can write and 
+//     // .and('contain','Button')
+//     // .and('have.id','query-btn')
+//     // .and('be.visible')
+
+// })
+
+//explicit assertion 
+
+// expect
+it ("explicit assertion",()=>{
     cy.visit('https://example.cypress.io')
     cy.wait(2000)
-    cy.contains('get').click()
-    cy.wait(2000)
-    cy.get('#query-btn')
-    .should('contain','Button')//should contain ->the text it have 
-    .should('have.id','query-btn')//should have .class .id .text .html
-    .should('be.visible')//should-be   .visible  .disabled be.focused == have.focus 
-    //should-equal  .invoke('attr','id').should('equal','class-name')
+    expect(true).to.be.true 
+    assert.strictEqual(4,'4','...')//false -> mean ===
 
-    //and -> chained assertion 
-    //instead of writing an should we can write and 
-    // .and('contain','Button')
-    // .and('have.id','query-btn')
-    // .and('be.visible')
 
 })
+//              .to.be.equal('')
+//              .to.be.a('string')
+//              .to.be.false
+//              .to.be.null
+//              .to.exist
+//              .to.be.a('string')
+
+// assert      
+// assert.equal(4,4,'...')//true
+// assert.equal(4,5,'...')//false
+// assert.equal(4,'4','...')//true
+// assert.strictEqual(4,'4','...')//false
+// assert.equal
+// assert.notEqual
+// assert.isAbove
+// assert.isBelow
+// assert.exists
+// assert.notExists
+// assert.true 
+// assert.false 
+// assert.isString 
+// assert.isNotNumber
+
