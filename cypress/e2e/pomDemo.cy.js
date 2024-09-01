e004bef1779bc9f0483e1124587be9446e77734b
sajeda swad <201047@ppu.edu.ps>
2024-09-01 18:55:07 +0300

    grouping test (describe ,skip ,only , beforEach

--- cypress/e2e/pomDemo.cy.js
+++ cypress/e2e/pomDemo.cy.js
@@ -1,15 +1,29 @@
 //page object model
 /// <reference types="cypress"/>
 
-import { LoginPage } from "./pages/login-page"
-var  loginPage  = new LoginPage() //create object of the class
+import { LoginPage } from "./pages/login-page";
+var loginPage = new LoginPage(); //create object of the class
 
-it ('POM DEMO',()=>{
+describe("all login test ", () => {
+  beforeEach(() => {
+    cy.visit("https://opensource-demo.orangehrmlive.com");
+  });
 
-    cy.visit('https://opensource-demo.orangehrmlive.com')
+  //it.only("login1", () => {it run just this test
+  //it.skip("login1", () => {it skip this test
+  it("login1", () => {
     //calling the class function
-    loginPage.enterUserName("admin")
-    loginPage.enterPassword("admin123")
-    loginPage.clickLogin()
-    
-})
+    loginPage.enterUserName("admin");
+    loginPage.enterPassword("admin123");
+    loginPage.clickLogin();
+    cy.get(":nth-child(1) > .oxd-main-menu-item").click();
+  });
+  it("login2", () => {
+    //calling the class function
+    //invalid credintional
+    loginPage.enterUserName("admin123");
+    loginPage.enterPassword("admin123");
+    loginPage.clickLogin();
+    cy.get(":nth-child(1) > .oxd-main-menu-item").click();
+  });
+});
