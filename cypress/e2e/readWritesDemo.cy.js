76b02835d5a1b3f1ce3a874f9c762f1a63d6baa9
sajeda swad <201047@ppu.edu.ps>
2024-09-02 12:09:13 +0300

    write file demo

--- cypress/e2e/readWritesDemo.cy.js
+++ cypress/e2e/readWritesDemo.cy.js
@@ -19,3 +19,13 @@ it("read file using readFile()", () => {
     cy.log(data.name);
   });
 });
+
+////
+it("write file demo", () => {
+  cy.writeFile("sample.txt", "hello im sajeda\n");
+  cy.writeFile("sample.txt", "hello im learning cypress", { flag: "a+" }); //to add this in file with out removing or overwrite on file
+  // cy.writeFile(
+  //   "or jsut write the relative path for the file i want to write on it ",
+  //   "hello im sajeda\n"
+  // );
+});
